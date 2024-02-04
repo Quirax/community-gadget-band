3c3b83ad2fd65ada6af7eec8a5ce98c7bd191233
Quirax Lee <quiraxical@gmail.com>
2024-02-04 17:41:06 +0900

    일반 로그인 기능 구현 - Redirect 수신할 페이지 구현

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,29 @@
 import { Route } from 'react-router-dom'
 
+function paramsToObject() {
+    const entries = new URLSearchParams(window.location.search).entries()
+
+    const result = {}
+    for (const [key, value] of entries) {
+        result[key] = value
+    }
+    return result
+}
+
+function loader() {
+    let query = paramsToObject()
+    console.debug('code', query)
+    return null
+}
+
 function Page(props) {
-    return <h1>Landing</h1>
+    return <p>Loading...</p>
 }
 
 export default (
     <Route
         path='login'
         element={<Page />}
+        loader={loader}
     />
 )
